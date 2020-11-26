bd350a6c0d5da698e28f396e7d80ba24510135ea
MichelLaplane <michel.laplane@hotmail.com>
2020-11-26 16:34:51 +0100

    V0.5.0.0 - added class component, functional components and keyboard hook management

--- src/webparts/reactClassFunctionHooks/components/ReactClassFunctionHooks.tsx
+++ src/webparts/reactClassFunctionHooks/components/ReactClassFunctionHooks.tsx
@@ -1,25 +1,84 @@
 import * as React from 'react';
+import { useEffect, useCallback, useReducer, useState } from "react";
 import styles from './ReactClassFunctionHooks.module.scss';
 import { IReactClassFunctionHooksProps } from './IReactClassFunctionHooksProps';
 import { escape } from '@microsoft/sp-lodash-subset';
 
-export default class ReactClassFunctionHooks extends React.Component<IReactClassFunctionHooksProps, {}> {
-  public render(): React.ReactElement<IReactClassFunctionHooksProps> {
+export interface IFunctionalComponentProps {
+  title: string;
+  keyPressed:string;
+}
+
+export const FunctionalComponent: React.FunctionComponent<IFunctionalComponentProps> = (props: IFunctionalComponentProps) => {
+
+  const [keyPressedValue,setKeyPressedValue] = useState("");
+
+  const keydownListener = useCallback(
+    keydownEvent => {
+      const { code, key,keyCode,ctrlKey,altKey,srcElement, target, repeat } = keydownEvent;
+      props.keyPressed = key;
+      console.log('You just pressed the key:' + key);
+      setKeyPressedValue(key);
+    },[]);
+
+  useEffect(() => {
+    window.addEventListener("keydown", keydownListener);
+    return () => window.removeEventListener("keydown", keydownListener);
+  }, []);
+  return (
+    <div>
+      <h1>{props.title}</h1>
+     <div>keyPressed props: {props.keyPressed}</div>
+     <div>keyPressedValue state:  {keyPressedValue}</div>
+     </div>
+  );
+};
+
+export interface IClassComponentProps {
+  title: string;
+}
+export interface IClassComponentState {
+  count: number;
+}
+
+class ClassComponent extends React.Component<IClassComponentProps,IClassComponentState> {
+  constructor(props: IClassComponentProps) {
+    super(props);
+    this.state = {
+      count: 0
+    };
+  }
+
+  public render(): React.ReactElement<IClassComponentProps> {
+
     return (
-      <div className={ styles.reactClassFunctionHooks }>
-        <div className={ styles.container }>
-          <div className={ styles.row }>
-            <div className={ styles.column }>
-              <span className={ styles.title }>Welcome to SharePoint!</span>
-              <p className={ styles.subTitle }>Customize SharePoint experiences using Web Parts.</p>
-              <p className={ styles.description }>{escape(this.props.description)}</p>
-              <a href="https://aka.ms/spfx" className={ styles.button }>
-                <span className={ styles.label }>Learn more</span>
-              </a>
-            </div>
-          </div>
-        </div>
+
+      <div>
+        <h1>{this.props.title}</h1>
+        <div>{this.state.count}</div>
+        <button onClick={() => this.setState({ count: this.state.count + 1 })}>Add</button>
       </div>
     );
   }
+
+}
+
+export default class ReactClassFunctionHooks extends React.Component<IReactClassFunctionHooksProps, {}> {
+
+  public render(): React.ReactElement<IReactClassFunctionHooksProps> {
+
+    return (
+
+      <div className={styles.reactClassFunctionHooks}>
+        <div className={styles.container}>
+          <div className={styles.row}>
+            <FunctionalComponent title="This is a function component" keyPressed=""></FunctionalComponent>
+            <ClassComponent title="This is a class component"></ClassComponent>
+          </div>
+        </div>
+      </div>
+
+    );
+  }
+
 }
